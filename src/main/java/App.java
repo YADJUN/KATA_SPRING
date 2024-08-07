63f1a22150890c9d39de3f292458b563f809fc38
YADJUN <162843765+YADJUN@users.noreply.github.com>
2024-08-07 04:40:22 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beane = (Cat) applicationContext.getBean("cat");
+        Cat beane1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean1);
+        System.out.println(beane == beane1);
+    }
+}
